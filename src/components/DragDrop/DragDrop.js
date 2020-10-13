950dff0cc73715e01b53865bdeef1f532a9c670a
Michelle Zhang <mwzhang@andrew.cmu.edu>
2020-10-13 11:16:56 -0400

    Drag Drop Chips

--- src/components/DragDrop/DragDrop.js
+++ src/components/DragDrop/DragDrop.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import styles from './DragDrop.css';
+import IconButton from '@material-ui/core/IconButton';
+import SaveAltIcon from '@material-ui/icons/SaveAlt';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
 
 export default class DragDrop extends React.Component {
   constructor(props) {
@@ -14,8 +18,9 @@ export default class DragDrop extends React.Component {
 
   render() {
     const schemaTitle = this.props.title;
+
     return (
-      <div>
+      <div className="container">
         <div className="title">
           <form>
             <input
@@ -23,12 +28,29 @@ export default class DragDrop extends React.Component {
               className="title-input"
               placeholder="Title Your Schema"
               value={schemaTitle} onChange={this.handleChange} />
+            <IconButton aria-label="save">
+              <SaveAltIcon />
+            </IconButton>
           </form>
         </div>
+
         <div className="dialog-area"></div>
-        <div className="components">
-            Components
+
+        <div className="component-container">
+          <h3>Components</h3>
+          <Grid container spacing={1}>
+            <Grid item md={2}>
+              <Paper children="State" elevation={3} className="large-paper" style={{backgroundColor: '#EDFEEA'}}/>
+            </Grid>
+            <Grid item md={2}>
+              <Paper children="Action" elevation={3} className="large-paper" style={{backgroundColor: '#FFF7D8'}}/>
+            </Grid>
+            <Grid item md={2}>
+              <Paper children="Query" elevation={3} className="large-paper" style={{backgroundColor: '#EAEEFE'}}/>
+            </Grid>
+          </Grid>
         </div>
+
       </div>
     )
   }
