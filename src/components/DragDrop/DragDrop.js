245a9298e9701158634001e4fad737c1eb8ec548
Michelle Zhang <mwzhang@andrew.cmu.edu>
2020-10-27 19:28:32 -0400

    Component Buttons Added

--- src/components/DragDrop/DragDrop.js
+++ src/components/DragDrop/DragDrop.js
@@ -1,15 +1,50 @@
 import React from 'react';
 import styles from './DragDrop.css';
+import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import SaveAltIcon from '@material-ui/icons/SaveAlt';
-import Paper from '@material-ui/core/Paper';
-import Grid from '@material-ui/core/Grid';
 import Flow from '../Flow/Flow';
 
 export default class DragDrop extends React.Component {
   constructor(props) {
     super(props);
     this.handleChange = this.handleChange.bind(this);
+    this.state = {
+      count: 4,
+      elementList: [
+        {
+          id: '1',
+          type: 'input',
+          data: {
+            label: (
+              <>
+                Welcome to the <strong>Dialog Builder</strong>
+              </>
+            ),
+          },
+          position: { x: 250, y: 0 },
+        },
+        {
+          id: '2',
+          data: {
+            label: 'drag me',
+          },
+          position: { x: 400, y: 350 },
+        },
+        {
+          id: '3',
+          type: 'output',
+          data: { label: 'try drawing a connection here' },
+          position: { x: 120, y: 350 },
+        },
+        {
+          id: 'e1-2',
+          source: '1',
+          target: '2',
+          label: 'to delete a node / edge: click and press delete key',
+          type: 'smoothstep',
+        }]
+    };
   }
 
   handleChange(e) {
@@ -36,11 +71,86 @@ export default class DragDrop extends React.Component {
         </div>
 
         <div className="component-container">
-          <div style={{ height: 600, width: 600 }}>
-            <Flow />
+          <div style={{ height: 450, width: 600 }}>
+            <Flow el={this.state.elementList} />
           </div>
         </div>
 
+        <div>
+          <h3>Add a Component</h3>
+          <span className="add-button">
+            <Button variant="outlined" color="primary"
+              onClick={() => {
+                const newEl = {
+                  id: this.state.count.toString(),
+                  type: 'default',
+                  data: { label: 'New State' },
+                  position: { x: 60, y: 50 },
+                  style: {
+                    border: '1px solid #454bff',
+                  }
+                };
+
+                this.setState(prevState => ({
+                  elementList: [...prevState.elementList, newEl],
+                  count: prevState.count + 1
+                }));
+                console.log(this.state.elementList);
+                console.log(this.state.count);
+              }}>
+              State
+          </Button>
+          </span>
+
+          <span className="add-button">
+            <Button variant="outlined" color="secondary"
+              onClick={() => {
+                const newEl = {
+                  id: this.state.count.toString(),
+                  type: 'default',
+                  data: { label: 'New Action' },
+                  position: { x: 60, y: 50 },
+                  style: {
+                    border: '1px solid #ff0a80',
+                  }
+                };
+
+                this.setState(prevState => ({
+                  elementList: [...prevState.elementList, newEl],
+                  count: prevState.count + 1
+                }));
+                console.log(this.state.elementList);
+                console.log(this.state.count);
+              }}>
+              Action
+          </Button>
+          </span>
+
+          <span className="add-button">
+            <Button variant="outlined" color="default"
+              onClick={() => {
+                const newEl = {
+                  id: this.state.count.toString(),
+                  type: 'default',
+                  data: { label: 'New Query' },
+                  position: { x: 60, y: 70 },
+                  style: {
+                    border: '1px solid #222222',
+                  }
+                };
+
+                this.setState(prevState => ({
+                  elementList: [...prevState.elementList, newEl],
+                  count: prevState.count + 1
+                }));
+                console.log(this.state.elementList);
+                console.log(this.state.count);
+              }}>
+              Query
+          </Button>
+          </span>
+
+        </div>
       </div>
     )
   }
--- src/components/Flow/Flow.js
+++ src/components/Flow/Flow.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState} from 'react';
 import ReactFlow, {
   removeElements,
   addEdge,
@@ -10,118 +10,13 @@ const onLoad = (reactFlowInstance) => {
   reactFlowInstance.fitView();
 };
 
-const Flow = () => {
-
-  const elementsList = [
-    {
-      id: '1',
-      type: 'input',
-      data: {
-        label: (
-          <>
-            Welcome to the <strong>Dialog Builder</strong>
-          </>
-        ),
-      },
-      position: { x: 250, y: 0 },
-    },
-    {
-      id: '2',
-      data: {
-        label: 'drag me',
-      },
-      position: { x: 250, y: 130 },
-    },
-
-    {
-      id: '3',
-      type: 'output',
-      data: { label: 'try drawing a connection here' },
-      position: { x: 120, y: 200 },
-    },
-    {
-      id: 'state',
-      type: 'default',
-      data: { label: 'New State' },
-      position: { x: 15, y: 400 },
-      style: {
-        border: '1px solid #454bff',
-      }
-    },
-    {
-      id: 'action',
-      type: 'default',
-      data: { label: 'New Action' },
-      position: { x: 215, y: 400 },
-      style: {
-        border: '1px solid #fc1e89',
-      },
-    },
-    {
-      id: 'query',
-      type: 'default',
-      data: { label: 'New Query' },
-      position: { x: 415, y: 400 },
-    },
-
-    {
-      id: 'e1-2',
-      source: '1',
-      target: '2',
-      label: 'to delete a node / edge: click and press delete key',
-      type: 'smoothstep',
-    },
-
-  ];
-
+const Flow = (props) => {
+  const elementsList = props.el;
   const [elements, setElements] = useState(elementsList);
+  console.log(elementsList);
 
   const onElementsRemove = (elementsToRemove) =>
     setElements((els) => removeElements(elementsToRemove, els));
-
-  const onNodeDragStart = (e, node) => {
-    if (node.position.x === 15 && node.position.y === 400) {
-      var newElements = elements.slice();
-
-      newElements.push({
-        id: node.id.concat('copy'),
-        type: 'default',
-        data: { label: 'New State' },
-        position: { x: 15, y: 400 },
-        style: {
-          border: '1px solid #454bff',
-        }
-      });
-      setElements(newElements);
-    }
-
-    if (node.position.x === 215 && node.position.y === 400) {
-      newElements = elements.slice();
-
-      newElements.push({
-        id: node.id.concat('copy'),
-        type: 'default',
-        data: { label: 'New Action' },
-        position: { x: 215, y: 400 },
-        style: {
-          border: '1px solid #fc1e89',
-        }
-      });
-      setElements(newElements);
-    }
-
-    if (node.position.x === 415 && node.position.y === 400) {
-      newElements = elements.slice();
-
-      newElements.push({
-        id: node.id.concat('copy'),
-        type: 'default',
-        data: { label: 'New Query' },
-        position: { x: 415, y: 400 },
-      });
-      setElements(newElements);
-    }
-  }
 
   const onConnect = (params) => setElements((els) => addEdge(params, els));
 
@@ -129,14 +24,11 @@ const Flow = () => {
     <ReactFlow
       elements={elements}
       onElementsRemove={onElementsRemove}
-      onNodeDragStart={onNodeDragStart}
       onConnect={onConnect}
       onLoad={onLoad}
-      snapToGrid={false}
-      snapGrid={[15, 15]}
     >
-      
+      <Controls />
     </ReactFlow>
   );
-};
+}
 export default Flow;
