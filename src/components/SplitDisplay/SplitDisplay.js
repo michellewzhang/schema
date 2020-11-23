76a9acd868a0e3609beca7e49848f806754db850
Michelle Zhang <mwzhang@andrew.cmu.edu>
2020-11-23 09:23:39 -0500

    Refresh Button & Auto-Refresh Implemented

--- src/components/SplitDisplay/SplitDisplay.js
+++ src/components/SplitDisplay/SplitDisplay.js
@@ -4,11 +4,9 @@ import SplitPane from 'react-split-pane';
 import DragDrop from '../DragDrop/DragDrop';
 import './SplitDisplay.css';
 
-
 export default class SplitDisplay extends React.Component {
   constructor(props) {
     const MY_USER_ID = Math.random().toString(36).substr(2, 9);
-
     super(props);
     this.onChange = this.onChange.bind(this);
     this.state = {
@@ -34,9 +32,20 @@ export default class SplitDisplay extends React.Component {
     return (
       <div className="split">
         <SplitPane allowResize={true} split="vertical">
-          <div className="dnd"><DragDrop onChange={this.onChange} title={this.state.title} userID={this.state.userID}
-            saveClicked={this.saveClicked} /></div>
-          <div><Messenger title={this.state.title} userID={this.state.userID} saveClicked={this.state.saveClicked} clickDone={this.clickDone} /></div>
+          <div className="dnd">
+            <DragDrop
+              onChange={this.onChange}
+              title={this.state.title}
+              userID={this.state.userID}
+              saveClicked={this.saveClicked} />
+          </div>
+          <div>
+            <Messenger
+              title={this.state.title}
+              userID={this.state.userID}
+              saveClicked={this.state.saveClicked}
+              clickDone={this.clickDone} />
+          </div>
         </SplitPane>
       </div>
     );
